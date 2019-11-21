a751084112b685f38d459d1edf892a7ae7a2f239
bing huang <1141392570@qq.com>
2019-11-21 16:58:46 +0800

    查询页写了一大半

--- src/main/java/com/how2java/servletCURD/AddHero.java
+++ src/main/java/com/how2java/servletCURD/AddHero.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @WebServlet(name = "addHero")
-public class addHero extends HttpServlet {
+public class AddHero extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String s = request.getParameter("data");
         JSONObject jb = JSONObject.parseObject(s);
---
+++ src/main/java/com/how2java/servletCURD/SearchHero.java
@@ -0,0 +1,42 @@
+package com.how2java.servletCURD;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import com.how2java.HeroDao.Hero;
+import com.how2java.HeroDao.HeroDAO;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet(name = "SearchHero")
+public class SearchHero extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        search(request, response);
+    }
+
+    private void search(HttpServletRequest requestm, HttpServletResponse response) throws IOException {
+//        查询数据库中所有键并放到list中
+        List<Hero> list = new HeroDAO().list();
+        String s = "";
+//        创建一个json数组，遍历list,把每一个对象转化为json对象,并添加到json数组中
+        JSONArray ja = new JSONArray();
+        for (Hero hero : list) {
+            ja.add(JSONObject.toJSON(hero));
+        }
+
+        response.setContentType("text/html;charset=UTF-8");
+        response.getWriter().print(JSONArray.toJSONString(ja));
+        System.out.println(JSONArray.toJSONString(ja));
+        System.out.println();
+    }
+}
